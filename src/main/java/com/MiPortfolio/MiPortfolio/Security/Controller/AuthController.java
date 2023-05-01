288750e6f5e28248072eb193b8dc7b6d8cb53dec
MarianoHerrlein <marianoherrlein@hotmail.com>
2023-05-01 02:56:51 -0300

    correcciones8

--- src/main/java/com/MiPortfolio/MiPortfolio/Security/Controller/AuthController.java
+++ src/main/java/com/MiPortfolio/MiPortfolio/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontend-d69bd.web.app"})
+@CrossOrigin(origins = {"https://backend-v7vt.onrender.com"})
 public class AuthController { 
 
     @Autowired
