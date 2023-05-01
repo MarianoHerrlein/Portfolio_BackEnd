21f9baa81c99e7eba6939d3a4a3eb2a72db52cf4
MarianoHerrlein <marianoherrlein@hotmail.com>
2023-05-01 02:15:16 -0300

    Correcciones7

--- src/main/java/com/MiPortfolio/MiPortfolio/Controller/CEducacion.java
+++ src/main/java/com/MiPortfolio/MiPortfolio/Controller/CEducacion.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-d69bd.web.app"})
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
