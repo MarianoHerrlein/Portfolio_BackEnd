7f56dbda1da73502a5f2d2432db3c3fbd2164e29
MarianoHerrlein <marianoherrlein@hotmail.com>
2023-05-03 10:40:03 -0300

    p8

--- src/main/java/com/MiPortfolio/MiPortfolio/Controller/PersonaController.java
+++ src/main/java/com/MiPortfolio/MiPortfolio/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontend-d69bd.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
 
     @Autowired
--- src/main/java/com/MiPortfolio/MiPortfolio/Security/Controller/AuthController.java
+++ src/main/java/com/MiPortfolio/MiPortfolio/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontend-d69bd.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class AuthController { 
 
     @Autowired
