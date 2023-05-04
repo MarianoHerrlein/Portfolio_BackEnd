af34567de0bd26ddaa2352b1c3151e915efaf79a
MarianoHerrlein <marianoherrlein@hotmail.com>
2023-05-03 22:54:55 -0300

    Prueba

--- src/main/java/com/MiPortfolio/MiPortfolio/Security/Controller/AuthController.java
+++ src/main/java/com/MiPortfolio/MiPortfolio/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"frontend-d69bd.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"frontend-d69bd.web.app","*"})
 public class AuthController { 
 
     @Autowired
